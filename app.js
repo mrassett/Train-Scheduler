8291dd58b824f62446ed2015882c8eb8cf94bf00
mrassett <magsrassett@gmail.com>
2018-04-01 20:29:59 -0500

    Next Arrival function remaining

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 let todaysDate = new Date(); 
-let datetime = todaysDate.getDate() + "/"
-            + (todaysDate.getMonth()+1)  + "/" 
-            + todaysDate.getFullYear() + " @ "  
-            + todaysDate.getHours() + ":"  
-            + todaysDate.getMinutes() + ":" 
-            + todaysDate.getSeconds();
+let dateTime = moment(todaysDate).format('MMMM Do YYYY, h:mm:ss a');
 
-document.write(datetime); 
-  
-  
-  
+$("#time-now").html(dateTime); 
+
   // Initialize Firebase
-  var config = {
+
+let config = {
     apiKey: "AIzaSyCqNNPbGEYokW6CsCvArHwzau7s2AOtR5E",
     authDomain: "train-scheduler-f8637.firebaseapp.com",
     databaseURL: "https://train-scheduler-f8637.firebaseio.com",
@@ -21,7 +15,7 @@ document.write(datetime);
   };
   firebase.initializeApp(config);
 
-  let database = firebase.database()
+  let database = new firebase.database()
 
   // Onclick function
   $("#submit").click(function(event) {
@@ -30,6 +24,7 @@ document.write(datetime);
     let destination = $("#input-destination").val().trim();
     let time = $("#input-train-time").val().trim();
     let frequency = $("#input-frequency").val().trim();
+   
 
 //create a local instance of the train
    
@@ -44,7 +39,12 @@ document.write(datetime);
 //push new train object to Firebase - database push
     database.ref().push(newTrain);
 
-    console.log(timeAdded);
+//clear function to clear values & allow new values to be entered
+    $("#input-name").val("");
+    $("#input-destination").val("");
+    $("#input-train-time").val("");
+    $("#input-frequency").val("");
+
 
 //clear inputs
 })
@@ -56,7 +56,7 @@ document.write(datetime);
 //similar to above,all things getting back from database, snapshot.val.name.etc 
 //available so that it can be avaible to the selector
 
-database.ref().on("child_added", function(snapshot) {
+database.ref().on("child_added", function(snapshot, prevChildKey) {
       console.log(snapshot.val());
       let storedName = snapshot.val().name;
       let storedDestination = snapshot.val().destination;
@@ -65,32 +65,30 @@ database.ref().on("child_added", function(snapshot) {
       console.log(storedName);
       console.log(storedArrival);
 
-
-firebase.database().ref().on("value", function(snapshot){
-    $("#input-name").html(snapshot.val().name);
-    $("#input-destination").html(snapshot.val().destination);
-    $("#input-train-time").html(snapshot.val().arrival);
-    $("#input-frequency").html(snapshot.val().frequency);
-   
     //clear function to clear values & allow new values to be entered
+
+    let timeAdded = snapshot.val().timeAdded;
+    let useDateTime = moment(todaysDate).format();
+    let minutes = moment().diff(moment.unix(timeAdded), "minutes") % storedFrequency;
+    let a = storedFrequency - minutes;
+    console.log(minutes);
+    let humanTime = moment.duration(a, "minutes").humanize();
+    console.log(humanTime);
+  
     //create table data & rows (html) 
-    $("#train-table > tbody").append("<tr><td>" + storedName + "</td><td>" + storedDestination + "</td><td>" +
-    storedArrival + "</td><td>" + storedFrequency + "</td><td>");
-
-    $("#input-name").val("");
-    $("#input-destination").val("");
-    $("#input-train-time").val("");
-    $("#input-frequency").val("");
+     $("#train-table > tbody").append("<tr><td>" + storedName + "</td><td>" + storedDestination + "</td><td>" +
+     storedFrequency + "</td><td>" + storedArrival + "</td><td>"+ humanTime + "</td><td>");
 
 
-//timestamp
-  moment().format('LTS');
 });
 
-});
+
 
 
 
 //manipulate numbers before re-appending 
 
 //moment.js to use time calcs
+
+//add amount of minutes to dateTime (value of frequency) to get the next arrival
+//if can't add to dateTime add to TIMESTAMP
